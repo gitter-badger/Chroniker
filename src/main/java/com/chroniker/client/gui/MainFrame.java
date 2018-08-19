2a62f76002070e2b94986d4dd605ea98073e71d5
TomB_134 <thomasbaum134@outlook.com>
2018-08-19 13:19:07 +1000

    Updated log4j config. Begin building program foundations

---
+++ src/main/java/com/chroniker/client/gui/MainFrame.java
@@ -0,0 +1,13 @@
+package com.chroniker.client.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainFrame extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
